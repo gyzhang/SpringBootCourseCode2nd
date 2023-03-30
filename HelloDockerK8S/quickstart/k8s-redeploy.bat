5071777511ce8bda3b584e67fc3435bdaade44ab
Kevin Zhang <xprogrammer@163.com>
2023-03-30 20:26:56 +0800

    更新order-console前端项目中nginx部署脚本使用ConfigMap提供反向代理的配置

--- HelloDockerK8S/quickstart/k8s-redeploy.bat
+++ HelloDockerK8S/quickstart/k8s-redeploy.bat
@@ -13,3 +13,10 @@ kubectl delete -n sbc2nd service rabbitmq
 kubectl delete -n sbc2nd configmap mysql-init-sql
 kubectl delete -n sbc2nd configmap nginx-config
 kubectl delete namespace sbc2nd
+kubectl create namespace sbc2nd
+kubectl apply -f ./k8s/k8s-rabbitmq.yaml
+kubectl apply -f ./k8s/k8s-redis.yaml
+kubectl apply -f ./k8s/k8s-mysql.yaml
+kubectl apply -f ./k8s/k8s-app-order-service.yaml
+kubectl apply -f ./k8s/k8s-app-order-console.yaml
+kubectl apply -f ./k8s/k8s-app-express.yaml
